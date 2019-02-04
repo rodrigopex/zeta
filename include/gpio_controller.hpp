ad96a4922abebc605d50780d6d768ebc21008849
Rodrigo Peixoto <rodrigopex@ayna.tech>
2019-02-03 21:31:19 -0300

    Initial commit for the lib base
    Signed-off-by: Rodrigo Peixoto <rodrigopex@ayna.tech>

---
+++ include/gpio_controller.hpp
@@ -0,0 +1,46 @@
+/**!
+ * @file gpio_controller.hpp
+ * @author Rodrigo Peixoto (rodrigopex@edge.ufal.br)
+ * @brief
+ * @version 0.1
+ * @date 2019-01-28
+ *
+ */
+
+#include <device.h>
+#include <gpio.h>
+#include <misc/printk.h>
+#include <zephyr.h>
+
+#define MAX_INPUT_PINS 4
+
+#include "pin_interface.hpp"
+
+
+class GPIOController
+{
+   public:
+    void init(PinInput *pin_input);
+    int add_callback(PinInput *pin_input);
+    static void changed(struct device *dev, struct gpio_callback *cb, u32_t pins)
+    {
+        GPIOController *c = GPIOController::instance();
+        for (u8_t i = 0; c->m_pins[i] && (i < MAX_INPUT_PINS); i++) {
+            if (BIT(c->m_pins[i]->pin()) & pins) {
+                c->m_pins[i]->callback();
+            }
+        }
+    }
+    static GPIOController *instance()
+    {
+        return &m_instance;
+    }
+
+   private:
+    GPIOController();
+    struct gpio_callback m_gpio_cb;
+    struct device *m_device;
+    PinInput *m_pins[MAX_INPUT_PINS];
+    static GPIOController m_instance;
+    u32_t m_pin_mask;
+};
