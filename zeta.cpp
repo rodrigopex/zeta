8847e371e44da79c12e093df0d03ba2116755a7f
Rodrigo Peixoto <rodrigopex@gmail.com>
2019-02-04 10:18:00 -0300

    Put all the code in single hpp and cpp files
    Signed-off-by: Rodrigo Peixoto <rodrigopex@gmail.com>

---
+++ zeta.cpp
@@ -0,0 +1,202 @@
+#include "zeta.hpp"
+
+namespace zt
+{
+u8_t *Property::data()
+{
+    return m_data;
+}
+int Property::setData(u8_t *data, size_t size)
+{
+    if (size == m_size) {
+        memcpy(m_data, data, size);
+        return 0;
+    }
+    return -EINVAL;
+}
+size_t Property::size()
+{
+    return m_size;
+}
+
+
+/* HardProperty */
+HardProperty::HardProperty() : m_observer(Observer(nullptr, 0))
+{
+}
+int HardProperty::write(u8_t state)
+{
+    return 0;
+}
+int HardProperty::notify()
+{
+    if (m_observer.property) {
+        switch (m_observer.method) {
+        case 'w':
+            m_observer.property->write(*m_data);
+            break;
+        case 't':
+            m_observer.property->toggle();
+            break;
+        case 'n':
+            m_observer.property->notify();
+            break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
+int HardProperty::toggle()
+{
+    return 0;
+}
+
+void HardProperty::connect(HardProperty *property, u8_t method)
+{
+    m_observer.property = property;
+    m_observer.method   = method;
+}
+
+/* DigitalInput */
+
+DigitalInput::DigitalInput(u8_t *data, const char *controller, u32_t pin) : m_pin(pin)
+{
+    m_data   = data;
+    *data    = 0;
+    m_device = device_get_binding(controller);
+    gpio_pin_configure(m_device, m_pin, this->configureFlags());
+}
+int DigitalInput::configureFlags()
+{
+    return (GPIO_PUD_PULL_UP | GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE
+            | GPIO_INT_ACTIVE_LOW);
+}
+int DigitalInput::read(u8_t *state)
+{
+    *state = *m_data;
+    return 0;
+}
+int DigitalInput::callback()
+{
+    u8_t state = 0;
+    int error  = gpio_pin_read(m_device, m_pin, (u32_t *) &state);
+    if (!error) {
+        *m_data = state;
+        this->notify();
+    }
+    return 0;
+}
+struct device *DigitalInput::device()
+{
+    return m_device;
+}
+u32_t DigitalInput::pin()
+{
+    return m_pin;
+}
+int DigitalInput::notify()
+{
+    printk("Triggered pin %d:%d\n", m_pin, (u8_t) *m_data);
+    HardProperty::notify();
+    return 0;
+}
+
+/* DigitalOutput */
+
+DigitalOutput::DigitalOutput(u8_t *data, const char *controller, u32_t pin) : m_pin(pin)
+{
+    m_data   = data;
+    *data    = 0;
+    m_device = device_get_binding(controller);
+    gpio_pin_configure(m_device, m_pin, this->configureFlags());
+}
+int DigitalOutput::configureFlags()
+{
+    return GPIO_DIR_OUT;
+}
+int DigitalOutput::read(u8_t *state)
+{
+    return gpio_pin_read(m_device, m_pin, (u32_t *) state);
+}
+int DigitalOutput::write(u8_t state)
+{
+    int error = gpio_pin_write(m_device, m_pin, state);
+    if (!error) {
+        *m_data = state;
+        this->notify();
+    }
+    return error;
+}
+int DigitalOutput::toggle()
+{
+    int error = gpio_pin_write(m_device, m_pin, !*m_data);
+    if (!error) {
+        *m_data = !*m_data;
+        this->notify();
+    }
+    return error;
+}
+int DigitalOutput::notify()
+{
+    printk("State changed to %d\n", *m_data);
+    HardProperty::notify();
+    return 0;
+}
+struct device *DigitalOutput::device()
+{
+    return m_device;
+}
+u32_t DigitalOutput::pin()
+{
+    return m_pin;
+}
+
+/* GPIOController */
+
+GPIOController GPIOController::m_instance = GPIOController();
+
+GPIOController::GPIOController() : m_device(nullptr)
+{
+    m_pin_mask = 0;
+    for (u8_t i = 0; i < MAX_INPUT_PINS; i++) {
+        m_pins[i] = nullptr;
+    }
+}
+void GPIOController::init(const char *controller)
+{
+    m_device = device_get_binding(controller);
+    if (!m_device) {
+        // ERROR
+    } else {
+        gpio_add_callback(m_device, &m_gpio_cb);
+    }
+}
+int GPIOController::add_callback(DigitalInput *pin_input)
+{
+    m_pin_mask = m_pin_mask | BIT(pin_input->pin());
+    gpio_init_callback(&m_gpio_cb, &GPIOController::changed, m_pin_mask);
+    printk("Error 2: %d\n", gpio_pin_enable_callback(m_device, pin_input->pin()));
+    s8_t available = -1;
+    bool exists    = false;
+    for (u8_t i = 0; i < MAX_INPUT_PINS; i++) {
+        if (m_pins[i] == nullptr) {
+            available = i;
+            break;
+        } else if (m_pins[i] == pin_input) {
+            exists = true;
+            break;
+        }
+    }
+    if (!exists) {
+        if (available >= 0) {
+            m_pins[available] = pin_input;
+            printk("Pin added at %d\n", available);
+        } else {
+            printk("No more room for this item\n");
+        }
+    }
+    return 0;
+}
+
+}  // namespace zt
---
+++ zeta.hpp
@@ -0,0 +1,141 @@
+/**!
+ * @file zeta.hpp
+ * @author Rodrigo Peixoto (rodrigopex@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2019-02-04
+ *
+ *
+ */
+#include <device.h>
+#include <gpio.h>
+#include <misc/printk.h>
+#include <string.h>
+#include <zephyr.h>
+
+#define ZT_ALLOC_BYTES(x) (u8_t *) alloca(x)
+#define ZT_ALLOC_BYTE() (u8_t *) alloca(1)
+
+#define MAX_INPUT_PINS 4
+
+typedef enum {
+    VOLATILE_FLAG  = 0,
+    IN_FLASH_FLAG  = 1 << 0,
+    READ_ONLY_FLAG = 1 << 1
+} PropertyFlag;
+
+namespace zt
+{
+class Property
+{
+   public:
+    virtual u8_t *data();
+    virtual int setData(u8_t *data, size_t size);
+    virtual size_t size();
+    virtual int notify() = 0;
+
+   protected:
+    size_t m_size;
+    u8_t *m_data;
+    u8_t m_changed;
+    u8_t m_flags;
+};
+
+class HardProperty;
+
+struct Observer {
+    HardProperty *property;
+    u8_t method;
+    Observer(HardProperty *property, u8_t method) : property(property), method(method)
+    {
+    }
+    Observer()
+    {
+        property = nullptr;
+        method   = 0;
+    }
+};
+
+class HardProperty : public Property
+{
+   public:
+    HardProperty();
+    virtual int notify();
+    virtual void connect(HardProperty *property, u8_t method);
+    virtual struct device *device() = 0;
+    virtual u32_t pin()             = 0;
+    virtual int configureFlags()    = 0;
+    virtual int read(u8_t *state)   = 0;
+    virtual int write(u8_t state);
+    virtual int toggle();
+
+   protected:
+    Observer m_observer;
+};
+
+
+class DigitalInput : public HardProperty
+{
+   protected:
+    struct device *m_device;
+    u32_t m_pin;
+
+   public:
+    DigitalInput(u8_t *data, const char *controller, u32_t pin);
+    int configureFlags();
+    int read(u8_t *state);
+    int callback();
+    struct device *device();
+    u32_t pin();
+
+    virtual int notify();
+};
+
+class DigitalOutput : public HardProperty
+{
+   protected:
+    struct device *m_device;
+    u32_t m_pin;
+
+   public:
+    DigitalOutput(u8_t *data, const char *controller, u32_t pin);
+    virtual int notify();
+    int configureFlags();
+    int read(u8_t *state);
+    int write(u8_t state);
+    int toggle();
+    struct device *device();
+    u32_t pin();
+};
+
+
+/* GPIOController */
+class GPIOController
+{
+   public:
+    void init(const char *controller);
+    int add_callback(zt::DigitalInput *pin_input);
+    static void changed(struct device *dev, struct gpio_callback *cb, u32_t pins)
+    {
+        GPIOController *c = GPIOController::instance();
+        for (u8_t i = 0; c->m_pins[i] && (i < MAX_INPUT_PINS); i++) {
+            if (BIT(c->m_pins[i]->pin()) & pins) {
+                c->m_pins[i]->callback();
+            }
+        }
+    }
+    static GPIOController *instance()
+    {
+        return &m_instance;
+    }
+
+   private:
+    GPIOController();
+    struct gpio_callback m_gpio_cb;
+    struct device *m_device;
+    zt::DigitalInput *m_pins[MAX_INPUT_PINS];
+    static GPIOController m_instance;
+    u32_t m_pin_mask;
+};
+
+}  // namespace zt
